f145dab9f335d5a0ca3d8248a24b44ec7e765759
linoblondelle <33087397+lnbodle@users.noreply.github.com>
2021-05-08 01:00:27 +0200

    Responsive

--- js/rotation.js
+++ js/rotation.js
@@ -1,4 +1,7 @@
 var circles = document.getElementsByClassName('window');
+
+
+
 var globalAngle = 0;
 var previousAngle = 0;
 var lerpAngle = 0;
@@ -10,47 +13,40 @@ var screenWidth = window.innerWidth ;
 var screenHeight = window.innerHeight;
 
 
-window.onload = function() {
-  //rotateArround();
-
-
-}
-
+//Creating angles list for magnet effect.
 var angles = [];
-for (var i = 0 ; i<Math.PI*2 ; i+= (Math.PI*2)/circles.length) {
+for (var i = 0 ; i<=Math.PI*2 ; i+= (Math.PI*2)/circles.length) {
   angles.push(i);
   console.log(angles);
 }
 
+//Main loop
 function animate() {
 
   globalAngle = globalAngle%(2*Math.PI);
-  if (globalAngle < 0) globalAngle = 2*Math.PI;
-   
+  if (globalAngle <= 0) globalAngle = 2*Math.PI;
+
+
+  index = Math.round(map_range(globalAngle,0,Math.PI*2,circles.length,0));
 
     rotateArround();
     
     if (!swiping) {
-      var is_clock = false;
-      if (swipDir > 0) {is_clock = true;} else {is_clock = false;} 
+      
 
       var wantedAngle = closest(globalAngle,angles);
-      globalAngle = lerp(globalAngle,wantedAngle,0.1);
+      /*if (wantedAngle == (2*Math.PI) ) {
+       
+        globalAngle = lerp(globalAngle,0,0.1);
+      } else {*/
+        globalAngle = lerp(globalAngle,wantedAngle,0.1);
+      /*}*/
+     
+    
       previousAngle = wantedAngle;
-    } else {
-      
-    }
-
-    /*if (globalAngle > 2*Math.PI) globalAngle = 0;
-    if (globalAngle < 0) globalAngle = Math.PI*2;*/
-
-
-
-  // Start next frame
+    } 
   requestAnimationFrame(animate);
 }
-
-// Start animation
 animate();
 
 var touchXstart = 0;
@@ -59,8 +55,130 @@ var touchYstart = 0;
 var touchXend = 0;
 var touchYend = 0;
 
+function actionStart(e) {
+  var touchobj = e.changedTouches[0];
+  touchXstart = touchobj.pageX;
+}
+
+function actionMove(e) {
+  var touchobj = e.changedTouches[0];
+  touchXend = touchobj.pageX;
+  swiping = true;
+  var dir = touchXend - touchXstart;
+  globalAngle = map_range(dir,-screenWidth/2,screenWidth/2,-Math.PI/2,Math.PI/2) + previousAngle;
+}
+
+function actionEnd(e) {
+  var touchobj = e.changedTouches[0];
+  previousAngle = globalAngle;
+  swipDir = Math.sign(touchXend - touchXstart);
+  swiping = false;
+}
+
+window.addEventListener('resize',function(e) {
+   screenWidth = window.innerWidth ;
+   screenHeight = window.innerHeight;
+});
+
+//On mobile
+window.addEventListener('touchstart',function(e) {
+  actionStart(e);
+});
+
+window.addEventListener('touchmove',function(e){
+  actionMove(e);
+});
+
+window.addEventListener('touchend',function(e){
+  actionEnd(e);
+});
 
 
+//On computer
+window.addEventListener('mousedown',function(e) {
+  var mouseobj = e;
+  touchXstart = mouseobj.pageX;
+  console.log("click");
+  swiping = true;
+});
+
+window.addEventListener('mousemove',function(e) {
+  if (!swiping) return;
+  var touchobj = e;
+  touchXend = touchobj.pageX;
+
+  var dir = touchXend - touchXstart;
+  globalAngle = map_range(dir,-screenWidth/2,screenWidth/2,-Math.PI/2,Math.PI/2) + previousAngle;
+  console.log("pressed");
+});
+
+window.addEventListener('mouseup',function(e) {
+  var touchobj = e;
+  previousAngle = globalAngle;
+  swipDir = Math.sign(touchXend - touchXstart);
+  swiping = false;
+  console.log("relased");
+});
+
+
+function rotateArround() {
+  for (var i = 0; i < circles.length; i++) {
+
+    var circle = circles[i];
+
+    var angle = map_range(i, 0, circles.length, 0, Math.PI * 2.0) + globalAngle;
+
+    var carousselWidth, carousselHeight;
+    if (window.innerWidth > 1024) {
+
+      carousselWidth =   screenWidth / 3;
+      carousselHeight =  screenHeight / 7;
+
+      circle.style.width = "17vw"
+      circle.style.height = "17vw"
+    } else {
+      carousselWidth =   screenWidth /2;
+      carousselHeight =  screenHeight / 7;
+      circle.style.width = "17vh"
+      circle.style.height = "17vh"
+    }
+
+    var x = Math.round((screenWidth  / 2 ) + Math.sin(angle) * carousselWidth)- circle.offsetWidth / 2;
+    var y = Math.round((screenHeight / 2 ) + Math.cos(angle) * carousselHeight- circle.offsetHeight / 2);
+
+    if (i==index) {
+      circle.classList.add('selected') 
+    } 
+    else 
+    {
+      if (circle.classList.contains('selected')) {
+          circle.classList.remove('selected');
+      }
+    }
+
+    var size = (1 + Math.sin(angle + Math.PI / 2) * 0.7);
+    size = map_range(size, -0.7, 1.7, 0, 1);
+   
+    var h = Math.round(size*100)/100;        
+  
+    circle.style.zIndex = Math.floor(y);
+    circle.style.opacity = h;
+    circle.style.filter = "blur(" + (1-(size))*16 + "px)"
+    circle.style.transform =  "translate("+(x)+"px,"+(y)+"px)" + "scale(" + h + ")" ;
+  }
+}
+
+
+
+//Utilitaries function
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+function lerp(start, end, amt) {
+    return (1 - amt) * start + amt * end
+}
+  
 function closest(needle, haystack) {
   return haystack.reduce((a, b) => {
       let aDiff = Math.abs(a - needle);
@@ -72,122 +190,4 @@ function closest(needle, haystack) {
           return bDiff < aDiff ? b : a;
       }
   });
-}
-
-window.addEventListener('touchstart',function(e) {
-  touchXstart = e.changedTouches[0].pageX;
-  console.log(touchXstart);
-});
-
-window.addEventListener('touchmove',function(e){
-  var touchobj = e.changedTouches[0]
-  touchXend = touchobj.pageX;
-
-  swiping = true;
-
-  var dir = touchXend - touchXstart;
-  globalAngle = map_range(dir,-screenWidth/2,screenWidth/2,-Math.PI/2,Math.PI/2) + previousAngle;
-});
-
-window.addEventListener('touchend',function(e){
-  var touchobj = e.changedTouches[0]
-
-  previousAngle = globalAngle;
-
-  swipDir = Math.sign(touchXend - touchXstart);
- // updateRotation(-swipDir);
-  console.log(globalAngle);
-  swiping = false;
-});
-
-
-
-function updateRotation(direction) {
- /* globalAngle -= ((2 * Math.PI) / circles.length) * direction;*/
-
-    index += direction;
-    if (index > circles.length-1) index = 0;
-    if (index < 0) index = circles.length-1;
-}
-
-window.onwheel = function() {
-   // updateRotation(detectMouseWheelDirection(););
-  }
-
-if ( window.addEventListener ) {
-    document.addEventListener( 'DOMMouseScroll', function( e ) {
-        /*var scrollDirection = detectMouseWheelDirection( e );*/
-      //  updateRotation(detectMouseWheelDirection(););
-
-    });
-}
-
-function rotateArround() {
-
- 
-  //lerp(lerpAngle, globalAngle, 0.12);
-  
-
-
-  for (var i = 0; i < circles.length; i++) {
-
-    var circle = circles[i];
-
-    var angle = map_range(i, 0, circles.length, 0, Math.PI * 2.0) + globalAngle;
-
-    var x = Math.round(screenWidth  / 2 + Math.sin(angle) * screenWidth / 3 - circle.offsetWidth / 2);
-    var y = Math.round(screenHeight / 2 - Math.cos(angle) * -screenWidth / 7 - circle.offsetHeight /2 ) ;
-
-    if (i==index) {
-      circle.classList.add('selected') 
-
-    } else {
-      if (circle.classList.contains('selected')) {
-          circle.classList.remove('selected');
-      }
-    }
-
-    var size = (1 + Math.sin(angle + Math.PI / 2) * 0.7);
-    size = map_range(size, -0.7, 1.7, 0, 1);
-   
-    var h = size*100;          // 556.845
-    h = Math.round(h); // 556
-    h = h/100; 
-
-    //console.log(h);
-
-    //size = map_range(y, -100, 100, 0, 1);
-
-    circle.style.zIndex = Math.floor(y);
-    circle.style.opacity = h;
-    circle.style.filter = "blur(" + (1-(size))*16 + "px)"
-    circle.style.transform =  "translate("+(x)+"px,"+(y)+"px)" + "scale(" + h + ")" ;
- 
-  }
-}
-
-function map_range(value, low1, high1, low2, high2) {
-    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
-}
-
-function lerp(start, end, amt) {
-    return (1 - amt) * start + amt * end
-}
-  
-function detectMouseWheelDirection(e) {
-    var delta = null,
-      direction = false;
-    if (!e) { // if the event is not provided, we get it from the window object
-      e = window.event;
-    }
-    if (e.wheelDelta) { // will work in most cases
-      delta = e.wheelDelta / 60;
-    }
-    if (e.detail) { // fallback for Firefox
-      delta = -e.detail / 2;
-    }
-    if (delta !== null) {
-      direction = delta > 0 ? -1 : 1;
-    }
-    return direction;
 }
